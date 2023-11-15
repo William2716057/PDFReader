b75bd1ed8aa25e24df34bd5f91e52dd4051afb03
William Spriggs <Willspri@gmail.com>
2023-11-16 08:28:50 +1300

    changed

--- PDFReader/PDFReader.py
+++ PDFReader/PDFReader.py
@@ -4,3 +4,15 @@ def extractFromPDF(pdf_file: str) -> [str]:
     with open(pdf_file, 'rb') as pdf:
         reader = PyPDF2.PdfFilereader(pdf, strict=False)
         pdf_text = []
+
+        for page in reader.pages:
+            content = page.extract_text()
+            pdf_text.append(content)
+
+        return pdf_text
+
+
+    if __name__ == '__main__':
+        extracted_text = extract_text_pdf('file.pdf')
+        for text in extracted_text:
+            print(text)
