b140ed7d4c5fb9b4e3fafce1e453803e8bcebe68
William Spriggs <Willspri@gmail.com>
2023-11-16 09:09:04 +1300

    comments added

--- PDFReader/PDFReader.py
+++ PDFReader/PDFReader.py
@@ -1,8 +1,8 @@
 import PyPDF2
-
-def extractFromPDF(pdf_file: str) -> [str]:
+#function to take text from file and return as string
+def extractFromPDF(pdf_file: str) -> list[str]:
     with open(pdf_file, 'rb') as pdf:
-        reader = PyPDF2.PdfFilereader(pdf, strict=False)
+        reader = PyPDF2.PdfFilereader(pdf, strict=False)#False will continue to run even if errors encountered
         pdf_text = []
 
         for page in reader.pages:
@@ -13,6 +13,8 @@ def extractFromPDF(pdf_file: str) -> [str]:
 
 
     if __name__ == '__main__':
-        extracted_text = extractFromPDF('file.pdf')
+        extracted_text = extractFromPDF('sample.pdf')
+        coffee_count = 0
+        #Iterates through and prints text to console
         for text in extracted_text:
             print(text)
