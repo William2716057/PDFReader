f47ab91ff6de2e7e38fe6067c79d8ef18acdc78d
William Spriggs <Willspri@gmail.com>
2023-11-16 08:27:09 +1300

    updated

--- PDFReader/PDFReader.py
+++ PDFReader/PDFReader.py
@@ -1,4 +1,6 @@
 import pyPDF2
 
 def extractFromPDF(pdf_file: str) -> [str]:
-    with open(pdf_file)
+    with open(pdf_file, 'rb') as pdf:
+        reader = PyPDF2.PdfFilereader(pdf, strict=False)
+        pdf_text = []
